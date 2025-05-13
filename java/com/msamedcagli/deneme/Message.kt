7eccc0076d5599c54470fdbcffecfd83707771c8
Muhammed Samed Çağlı <136382731+msamedcagli@users.noreply.github.com>
2025-05-13 10:49:48 +0300

    Update Message.kt

--- java/com/msamedcagli/deneme/Message.kt
+++ java/com/msamedcagli/deneme/Message.kt
@@ -1,6 +1,9 @@
 package com.msamedcagli.deneme
 
+import android.net.Uri
+
 data class Message(
     val role: String,
-    val content: String
+    val content: String,
+    val imageUri: Uri? = null
 )
